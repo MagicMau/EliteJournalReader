643a7ce1e9261e7a482ffccd0c2dc9b1e47c34fc
Maurits Elbers <mau@mauilse.nl>
2019-01-21 13:08:51 +0100

    Replaced shared assembly info with local version.

--- EliteJournalReader/Properties/AssemblyInfo.cs
+++ EliteJournalReader/Properties/AssemblyInfo.cs
@@ -9,3 +9,22 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7790285b-e2ee-440b-8ea6-d27bbe6006d9")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MagicMau")]
+[assembly: AssemblyProduct("EliteJournalReader")]
+[assembly: AssemblyCopyright("Copyright © Maurits Elbers (@MagicMau) 2015-2019. Elite: Dangerous and related material is © Frontier Developments plc")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("3.3.1")]
+[assembly: AssemblyFileVersion("3.3.1")]
