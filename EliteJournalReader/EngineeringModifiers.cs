259d0769e96c029a4d02b9b62aed8595ccdc6b5f
Maurits Elbers <mau@mauilse.nl>
2019-12-09 20:10:43 +0100

    fix a parsing error with Engineering modifiers

--- EliteJournalReader/EngineeringModifiers.cs
+++ EliteJournalReader/EngineeringModifiers.cs
@@ -13,8 +13,10 @@ namespace EliteJournalReader
 
         public double Value { get; set; }
         public string ValueStr { get; set; }
+        public string ValueStr_Localised { get; set; }
         public double OriginalValue { get; set; }
         public string OriginalValueStr { get; set; }
+        public string OriginalValueStr_Localised { get; set; }
         public bool LessIsGood { get; set; }
     }
 
@@ -31,23 +33,33 @@ namespace EliteJournalReader
             mod.LessIsGood = obj.Value<bool>(nameof(mod.LessIsGood));
 
             var valueToken = obj[nameof(mod.Value)];
-            if (valueToken.Type == JTokenType.Float)
+            if (valueToken?.Type == JTokenType.Float)
             {
                 mod.Value = valueToken.Value<double>();
             }
-            else if (valueToken.Type == JTokenType.String)
+            else if (valueToken?.Type == JTokenType.String)
             {
                 mod.ValueStr = valueToken.Value<string>();
             }
+            else if (valueToken == null)
+            {
+                mod.ValueStr = obj[nameof(mod.ValueStr)]?.Value<string>();
+                mod.ValueStr_Localised = obj[nameof(mod.ValueStr_Localised)]?.Value<string>();
+            }
 
             var origToken = obj[nameof(mod.OriginalValue)];
-            if (origToken.Type == JTokenType.Float)
+            if (origToken?.Type == JTokenType.Float)
             {
                 mod.OriginalValue = origToken.Value<double>();
             }
-            else if (origToken.Type == JTokenType.String)
+            else if (origToken?.Type == JTokenType.String)
             {
                 mod.OriginalValueStr = origToken.Value<string>();
+            }
+            else if (origToken == null)
+            {
+                mod.OriginalValueStr = obj[nameof(mod.OriginalValueStr)]?.Value<string>();
+                mod.OriginalValueStr_Localised = obj[nameof(mod.OriginalValueStr_Localised)]?.Value<string>();
             }
 
             return mod;
