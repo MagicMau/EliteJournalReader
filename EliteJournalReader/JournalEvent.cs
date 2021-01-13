77fc74f38c519c0ed98c4a88cb4af76a5ea1c7c2
Barry "BarmyArmy" Wimlett <github@endless.co.uk>
2021-01-13 17:10:56 +0000

    Found some events which has 'unexpected' and useful SystemAddresses
    Found in the wild in real journal files and populated correctly - but not in the v25 documentation
    
    Modification to JounralEvent to detect jsonProperties in events which aren't in the eventArgs
    (maybe not useful for production - wrapped ion #ifdebugs )

--- EliteJournalReader/JournalEvent.cs
+++ EliteJournalReader/JournalEvent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 
 namespace EliteJournalReader
 {
@@ -35,10 +37,37 @@ namespace EliteJournalReader
         internal override JournalEventArgs FireEvent(object sender, JObject evt)
         {
             var eventArgs = evt.ToObject<TJournalEventArgs>();
+
             eventArgs.OriginalEvent = evt;
             eventArgs.Timestamp = DateTime.Parse(evt.Value<string>("timestamp"),
                 CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal);
 
+#if true
+            Type argsType = typeof(TJournalEventArgs);
+            var eventName = evt["event"];
+
+            var argsPropertyNames = argsType.GetProperties().Select(p => p.Name).ToList();
+            string[] ignoreProperties=new string[]{"event"};
+            foreach (var jProperty in evt.Properties())
+            {
+                var jsonPropertyName = jProperty.Name;
+                if (ignoreProperties.Contains(jsonPropertyName))
+                {
+                    // ignore anything in the ignore list
+                }
+                else if (jsonPropertyName.EndsWith("_Localised",StringComparison.CurrentCultureIgnoreCase))
+                {
+                    // ignore localised
+                } 
+                else if (!argsPropertyNames.Any(x=>string.Compare(jsonPropertyName,x,StringComparison.InvariantCultureIgnoreCase)==0))
+                {
+                    // found something missing
+                    Trace.TraceInformation($"EventArgs for {eventName} does not contain property {jsonPropertyName}");
+                    //Debugger.Break();
+                }
+
+            }
+#endif
             eventArgs.PostProcess(evt);
 
             Fired?.Invoke(sender, eventArgs);
