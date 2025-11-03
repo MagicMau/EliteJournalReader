1c97e91491204f7226abc51051abe7a8498e0942
Maurits Elbers <magicmau@gmail.com>
2025-11-03 14:40:37 +0100

    add raw json to MessageReceivedEventArgs

--- EliteJournalReader/MessageReceivedEventArgs.cs
+++ EliteJournalReader/MessageReceivedEventArgs.cs
@@ -3,11 +3,12 @@ using System;
 
 namespace EliteJournalReader
 {
-    public class MessageReceivedEventArgs(JournalEventArgs args, string eventType) : EventArgs
+    public class MessageReceivedEventArgs(JournalEventArgs args, string eventType, string json) : EventArgs
     {
         public JToken JToken { get; private set; } = args.OriginalEvent?.DeepClone();
         public string EventType { get; private set; } = eventType;
         public DateTime Timestamp { get; private set; } = args.Timestamp;
         public JournalEventArgs EventArgs { get; private set; } = args;
+        public string Json { get; private set; } = json;
     }
 }
